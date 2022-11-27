786e86dddd9a6cb6cddfbfe9668602f518203c66
Bhuvaneshwara Raja <bhuvaneshwara.raja@ei.study>
2022-11-27 19:19:07 +0530

    adding docker file

--- db/connection.js
+++ db/connection.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 
 const URI = "mongodb+srv://Netfilms:bhuvanesh@netfilms.7efmj.mongodb.net/user?retryWrites=true&w=majority"
 const connectDB = () => {
-    mongoose.connect(URI,{
-        useUnifiedTopology:true,
-        useNewUrlParser:true
-    })
-    console.log("db connected")
+    mongoose 
+    .connect(URI, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           useCreateIndex: true,   })   
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
+   
 }
 
 module.exports = connectDB
